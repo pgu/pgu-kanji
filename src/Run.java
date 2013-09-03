4d5e8c798e489325223d48b6a05b02f8ab540b26
Pascal Guilcher <guilcher.p@sfeir.com>
2013-09-03 18:25:06 +0200

    ..

--- src/Run.java
+++ src/Run.java
@@ -43,4 +43,50 @@ public class Run {
         System.out.println("Done!");
     }
 
+
+//    NodeList listOfPersons = doc.getElementsByTagName("person");
+//    int totalPersons = listOfPersons.getLength();
+//    System.out.println("Total no of people : " + totalPersons);
+//
+//    for(int s=0; s<listOfPersons.getLength() ; s++){
+//
+//
+//        Node firstPersonNode = listOfPersons.item(s);
+//        if(firstPersonNode.getNodeType() == Node.ELEMENT_NODE){
+//
+//
+//            Element firstPersonElement = (Element)firstPersonNode;
+//
+//            //-------
+//            NodeList firstNameList = firstPersonElement.getElementsByTagName("first");
+//            Element firstNameElement = (Element)firstNameList.item(0);
+//
+//            NodeList textFNList = firstNameElement.getChildNodes();
+//            System.out.println("First Name : " +
+//                    ((Node)textFNList.item(0)).getNodeValue().trim());
+//
+//            //-------
+//            NodeList lastNameList = firstPersonElement.getElementsByTagName("last");
+//            Element lastNameElement = (Element)lastNameList.item(0);
+//
+//            NodeList textLNList = lastNameElement.getChildNodes();
+//            System.out.println("Last Name : " +
+//                    ((Node)textLNList.item(0)).getNodeValue().trim());
+//
+//            //----
+//            NodeList ageList = firstPersonElement.getElementsByTagName("age");
+//            Element ageElement = (Element)ageList.item(0);
+//
+//            NodeList textAgeList = ageElement.getChildNodes();
+//            System.out.println("Age : " +
+//                    ((Node)textAgeList.item(0)).getNodeValue().trim());
+//
+//            //------
+//
+//
+//        }//end of if clause
+//
+//
+//    }//end of for loop with s var
+
 }
