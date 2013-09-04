5b420bf478973e6f77910b9667e72e250b29d7d1
Guilcher Pascal <pgu.dev@gmail.com>
2013-09-04 09:07:25 +0200

    ..

--- src/Run.java
+++ src/Run.java
@@ -11,7 +11,6 @@ import java.io.PrintWriter;
 public class Run {
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        System.out.println("toto");
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setIgnoringComments(true);
